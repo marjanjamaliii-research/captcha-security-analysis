cc44d6afcf95e402b03290c1843ec50d8e8fb78d
marjanjamaliii-research <marjan.jamaliii@gmail.com>
2025-11-29 02:36:19 +0330

    Update create_templates.m

--- src/create_templates.m
+++ src/create_templates.m
@@ -1,34 +1,14 @@
-%CREATE TEMPLATES
-%Letter
-A=imread('letters_numbers\A.bmp');B=imread('letters_numbers\B.bmp');
-C=imread('letters_numbers\C.bmp');D=imread('letters_numbers\D.bmp');
-E=imread('letters_numbers\E.bmp');F=imread('letters_numbers\F.bmp');
-G=imread('letters_numbers\G.bmp');H=imread('letters_numbers\H.bmp');
-I=imread('letters_numbers\I.bmp');J=imread('letters_numbers\J.bmp');
-K=imread('letters_numbers\K.bmp');L=imread('letters_numbers\L.bmp');
-M=imread('letters_numbers\M.bmp');N=imread('letters_numbers\N.bmp');
-O=imread('letters_numbers\O.bmp');P=imread('letters_numbers\P.bmp');
-Q=imread('letters_numbers\Q.bmp');R=imread('letters_numbers\R.bmp');
-S=imread('letters_numbers\S.bmp');T=imread('letters_numbers\T.bmp');
-U=imread('letters_numbers\U.bmp');V=imread('letters_numbers\V.bmp');
-W=imread('letters_numbers\W.bmp');X=imread('letters_numbers\X.bmp');
-Y=imread('letters_numbers\Y.bmp');Z=imread('letters_numbers\Z.bmp');
-%Number
-one=imread('letters_numbers\1.bmp');  two=imread('letters_numbers\2.bmp');
-three=imread('letters_numbers\3.bmp');four=imread('letters_numbers\4.bmp');
-five=imread('letters_numbers\5.bmp'); six=imread('letters_numbers\6.bmp');
-seven=imread('letters_numbers\7.bmp');eight=imread('letters_numbers\8.bmp');
-nine=imread('letters_numbers\9.bmp'); zero=imread('letters_numbers\0.bmp');
-%*-*-*-*-*-*-*-*-*-*-*-
-letter=[A B C D E F G H I J K L M...
-    N O P Q R S T U V W X Y Z];
-number=[one two three four five...
-    six seven eight nine zero];
-character=[letter number];
-templates=mat2cell(character,42,[24 24 24 24 24 24 24 ...
-    24 24 24 24 24 24 24 ...
-    24 24 24 24 24 24 24 ...
-    24 24 24 24 24 24 24 ...
-    24 24 24 24 24 24 24 24]);
-save ('templates','templates')
-clear all
+% CREATE TEMPLATES - Numbers Only
+zero = imread('letters/0.bmp');
+one  = imread('letters/1.bmp');
+two  = imread('letters/2.bmp');
+three= imread('letters/3.bmp');
+four = imread('letters/4.bmp');
+five = imread('letters/5.bmp');
+six  = imread('letters/6.bmp');
+seven= imread('letters/7.bmp');
+eight=imread('letters/8.bmp');
+nine = imread('letters/9.bmp');
+
+templates = mat2cell([zero one two three four five six seven eight nine], 42, repmat(24,1,10));
+save('templates','templates')
