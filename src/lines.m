19301f4c99a20bfe7002a31a9cb74d8da06b1f55
marjanjamaliii-research <marjan.jamaliii@gmail.com>
2025-11-29 01:46:02 +0330

    Create lines.m

---
+++ src/lines.m
@@ -0,0 +1,23 @@
+function [fl re]=lines(im_texto)
+% Divide text in lines
+% im_texto->input image; fl->first line; re->remain line
+im_texto=clip(im_texto);
+num_filas=size(im_texto,1);
+for s=1:num_filas
+    if sum(im_texto(s,:))==0
+        nm=im_texto(1:s-1, :); % First line matrix
+        rm=im_texto(s:end, :);% Remain line matrix
+        fl = clip(nm);
+        re=clip(rm);
+        
+        break
+    else
+        fl=im_texto;%Only one line.
+        re=[ ];
+    end
+end
+ 
+function img_out=clip(img_in)
+[f c]=find(img_in);
+img_out=img_in(min(f):max(f),min(c):max(c));%Crops image
+
